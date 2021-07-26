f1dc8787a4f3dc6ede9b49f9e33d14183d210e47
Admin <Admin@DESKTOP-JD7A7FJ>
2021-07-26 18:43:32 +0300

    Доработал защиту от дурака

---
+++ Geniy_Idiot_Finally/Geniy_Idiot_Common/UserResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+//Содержит информацию,которая хранится в файле
+//(Для отображения)
+namespace Geniy_Idiot_Common
+{
+    
+    public class UserResult
+    {
+        private static string userResultsPath = "userResults.json";
+        public string Name;
+        public int CountRightAnswers;
+        public string Diagnos;
+
+        public UserResult( string name, int countRightAnswers, string diagnos)
+        {
+            this.Name = name;
+            this.CountRightAnswers = countRightAnswers;
+            this.Diagnos = diagnos;
+        }
+
+        public  static void CreateFileResultsIfNotExists()
+        {
+            
+            if (!FileProvider.IsExists(userResultsPath))
+            {
+                var value = JsonConvert.SerializeObject(new List<UserResult>(), Formatting.Indented);
+                FileProvider.Add(userResultsPath, value);
+            }
+        }
+
+        public static void SaveResults(List<UserResult> results)
+        {
+            var serializedResults = JsonConvert.SerializeObject(results, Formatting.Indented);
+            FileProvider.Set(userResultsPath, serializedResults);
+
+        }
+
+        public static List<UserResult> GetResultsFromFile()
+        {
+            //Считываем содержимое файла
+            var serializedResults = FileProvider.Get(userResultsPath);
+            //Диссериализация(возвращает указанный тип данных)
+            var results = JsonConvert.DeserializeObject<List<UserResult>>(serializedResults);
+            return results;
+
+        }
+
+        
+
+    }
+}
